0f3a28e7c3b492bc882f3ba759c744ae1b987358
Aydıner Akdağ <arsarys@gmail.com>
2020-03-11 19:06:56 +0300

    Prologue eklendi slow mo ya kadar
    Prologue eklendi slow mo ya kadar

---
+++ JWE/Assets/Jack Walker/Game/Prologue Assets/ThrowHand/FlowHandThrow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowHandThrow : MonoBehaviour
+{
+    [SerializeField] FlowManager flowManager;
+
+    private void Update()
+    {
+        if(flowManager.flowPhase >= 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Hand"))
+        {
+            if (flowManager.flowPhase == 4)
+            {
+                flowManager.NextFlowLevel();
+            }
+            
+        }
+    }
+}
